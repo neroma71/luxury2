719eff68d2b559f4923e18e85a8eb920974c14e7
neroma71 <neroma71@gmail.com>
2023-09-19 12:07:47 +0200

    setup

---
+++ src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+   
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name'),
+            IntegerField::new('price'),
+        ];
+    }
+  
+}
